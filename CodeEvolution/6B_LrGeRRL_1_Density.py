f85554176360550532c76538974d8d03ae449fe0
ShirsenduP <s.podder@live.co.uk>
2019-08-10 21:47:25 +0100

    Tweaked density tests for regular random lattices, testing on larger range of degrees

--- CodeEvolution/6B_LrGeRRL_1_Density.py
+++ CodeEvolution/6B_LrGeRRL_1_Density.py
@@ -4,21 +4,21 @@ from CodeEvolution.Experiment import Experiment
 
 if __name__ == '__main__':
 
-    """Test 6 - Strategy 0 vs All-D. Testing d-Regular Random Lattices with d = [3, 4, ..., 10]."""
+    """Test 6 - Strategy 0 vs All-D. Testing d-Regular Random Lattices with d = [3, 4, ..., 20]."""
 
     rerunSimulations = True
     repeats = 100
 
     # Testing degree(density)
     var = 'degree'
-    values = [3, 4, 5, 6, 7, 8, 9, 10]
+    values = list(range(2, 20, 1))
     mainID = 1
 
     if rerunSimulations:
 
         # Run each experiment for each of the leading 8
         # Default Parameters for simulations
-        C = Config(size=200, sparseDensity=True, mutationProbability=0.1, maxPeriods=2000,
+        C = Config(size=200, mutationProbability=0.1, maxPeriods=5000,
                    initialState=State(mainID=mainID, proportion=1, mutantID=8))
         E = Experiment(networkType=LrGeRRLNetwork, defaultConfig=C, repeats=repeats,
                        variable=var, values=values)
--- CodeEvolution/6D_LrGeRRL_3_Density.py
+++ CodeEvolution/6D_LrGeRRL_3_Density.py
@@ -1,34 +1,26 @@
 from CodeEvolution.models import LrGeRRLNetwork
 from CodeEvolution.config import Config, State
 from CodeEvolution.Experiment import Experiment
-from CodeEvolution.Evaluator import Evaluator as E
 
 if __name__ == '__main__':
 
-    """Test 6 - Strategy 0 vs All-D. Testing d-Regular Random Lattices with d = [3, 4, ..., 10]."""
+    """Test 6 - Strategy 0 vs All-D. Testing d-Regular Random Lattices with d = [3, 4, ..., 20]."""
 
-    rerunSimulations = False
+    rerunSimulations = True
     repeats = 100
 
     # Testing degree(density)
     var = 'degree'
-    values = [3, 4, 5, 6, 7, 8, 9, 10]
-    mainID = 0
+    values = list(range(2, 20, 1))
+    mainID = 3
 
     if rerunSimulations:
 
         # Run each experiment for each of the leading 8
         # Default Parameters for simulations
-        C = Config(size=200, sparseDensity=True, mutationProbability=0.1, maxPeriods=2000,
+        C = Config(size=200, mutationProbability=0.1, maxPeriods=5000,
                    initialState=State(mainID=mainID, proportion=1, mutantID=8))
         E = Experiment(networkType=LrGeRRLNetwork, defaultConfig=C, repeats=repeats,
                        variable=var, values=values)
         E.showExperiments()
         E.run(export=True)
-
-    strategy0 = E.open_results("LrGeRRL_degree_2019-08-07 13:26:23")
-    # strategy0 = E.plotAllStrategiesSummary(title="", dataPath="LrGeRRL_degree_2019-08-07 13:26:23", mutantID=8)
-    print(strategy0[0])
-
-# TODO Rename script name from dominance to degree
-
