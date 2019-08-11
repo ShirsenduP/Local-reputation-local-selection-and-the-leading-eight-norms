c0e8da712840f0d02cc7a3a1e2957d341111cfc3
ShirsenduP <s.podder@live.co.uk>
2019-08-11 21:40:36 +0100

    Added bash scripts for cluster running

---
+++ Experiments/8A_6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+
+# Force bash as executing shell
+#$ -S /bin/bash
+
+# Request x  minutes (h:m:s)
+#$ -l h_rt=24:00:0
+
+# Request xGB memory (x = int)
+#$ -l mem=1G
+
+# Request XGB TMPDIR space (default - 10GB)
+#$ -l tmpfs=10G
+
+# Name of job
+#$ -N E_8A6
+
+# Set working dir
+#$ -wd /home/ucabpod/Scratch/TestOutput
+
+# go to tempdir (mandatory)
+cd $TMPDIR
+
+# run application
+module load python3/recommended
+source /home/ucabpod/Scratch/CodeEvolution/env/bin/activate
+/usr/bin/time --verbose python /home/ucabpod/Scratch/CodeEvolution/Experiments/8A_6.py
+
+# archive all files to scratch
+tar zcvf $HOME/Scratch/files_from_job_$JOB_ID.tar.gz $TMPDIR
+
+
