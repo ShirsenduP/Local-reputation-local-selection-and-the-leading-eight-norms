7b9a791bb6c565a5bae8220ac5230765464dfdf2
ShirsenduP <s.podder@live.co.uk>
2019-09-05 11:22:10 +0100

    Doing leftovers from exp 6C

--- CodeEvolution/ExperimentAnalysis/6B_L8-degree-ER/6B_L8-degree-ER.py
+++ CodeEvolution/ExperimentAnalysis/6B_L8-degree-ER/6B_L8-degree-ER.py
@@ -36,26 +36,26 @@ if __name__ == '__main__':
     plt.xticks(degrees, degrees, rotation=90)
     plt.legend(bbox_to_anchor=(0.45, 0.35))
     plt.tight_layout()
-    plt.savefig('6B_rrl_proportion')
-    # plt.show()
+    # plt.savefig('6B_rrl_proportion')
+    plt.show()
 
-    fig, ax = plt.subplots()
-    for ID, strategyID in zip(jobIDs, strategyIDs):
-
-        data = getDataFromID(ID)
-        length = data[0].shape[0]
-        means = [round(table['Prop. of Cooperators'].mean(), 5) for _, table in data.items()]
-        stds = [round(table['Prop. of Cooperators'].std() / np.sqrt(length), 5) for _, table in data.items()]
-        up = [mean + std for mean, std in zip(means, stds)]
-        down = [mean - std for mean, std in zip(means, stds)]
-        plt.plot(degrees, means, label=f'$s_{strategyID}$')
-        plt.fill_between(degrees, down, up, alpha=0.1, antialiased=True)
-
-    plt.title(f'LrGeRRL vs Number of neighbours $d$')
-    plt.xlabel('Number of neighbours $d$')
-    plt.ylabel('Average Proportion of Cooperation')
-    plt.xticks(degrees, degrees, rotation=90)
-    plt.legend(bbox_to_anchor=(0.45, 0.35), )
-    plt.tight_layout()
-    # plt.show()
-    plt.savefig('6B_rrl_cooperation')
+    # fig, ax = plt.subplots()
+    # for ID, strategyID in zip(jobIDs, strategyIDs):
+    #
+    #     data = getDataFromID(ID)
+    #     length = data[0].shape[0]
+    #     means = [round(table['Prop. of Cooperators'].mean(), 5) for _, table in data.items()]
+    #     stds = [round(table['Prop. of Cooperators'].std() / np.sqrt(length), 5) for _, table in data.items()]
+    #     up = [mean + std for mean, std in zip(means, stds)]
+    #     down = [mean - std for mean, std in zip(means, stds)]
+    #     plt.plot(degrees, means, label=f'$s_{strategyID}$')
+    #     plt.fill_between(degrees, down, up, alpha=0.1, antialiased=True)
+    #
+    # plt.title(f'LrGeRRL vs Number of neighbours $d$')
+    # plt.xlabel('Number of neighbours $d$')
+    # plt.ylabel('Average Proportion of Cooperation')
+    # plt.xticks(degrees, degrees, rotation=90)
+    # plt.legend(bbox_to_anchor=(0.45, 0.35), )
+    # plt.tight_layout()
+    # # plt.show()
+    # plt.savefig('6B_rrl_cooperation')
