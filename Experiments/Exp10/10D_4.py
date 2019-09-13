b251f7fa4cf09ee59cd7919c1ef014aa2df0a32f
ShirsenduP <s.podder@live.co.uk>
2019-09-13 12:22:15 +0100

    Added exp 10 on the other networks

---
+++ Experiments/Exp10/10D_4.py
@@ -0,0 +1,32 @@
+from CodeEvolution.models import LrGeRRLNetwork
+from CodeEvolution.config import Config, State
+from CodeEvolution.Experiment import Experiment
+import numpy as np
+
+if __name__ == '__main__':
+
+    """
+    Test 10A - 0
+    
+    Effect of increasing the mutation rate on LrGeERNetwork to see where the threshold is for mutation to dominate
+    Range from 0.1, 0.5 - 10 in 0.5 increments. Recall this parameter represents the expected number of 
+    mutations/time-step.
+
+    """
+
+    rerunSimulations = True
+    repeats = 100
+    var = 'mutationProbability'
+    values = list(np.arange(0, 10.01, 1))
+    mainID = 4
+
+    if rerunSimulations:
+
+        # Default Parameters for simulations
+        C = Config(size=300, initialState=State(mainID, 1, 8), mutationProbability=0.1, degree=4,
+                   maxPeriods=2500)
+        E = Experiment(networkType=LrGeRRLNetwork, defaultConfig=C, repeats=repeats,
+                       variable=var, values=values)
+
+        E.showExperiments()
+        E.run(cluster=True)
