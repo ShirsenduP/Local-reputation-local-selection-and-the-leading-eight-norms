e8193a118e867a6a1049b59c066c150ed195a5fd
ShirsenduP <s.podder@live.co.uk>
2020-01-13 01:00:19 +0000

    Removed oct2py from required packages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="CodeEvolution",
-	version="1.0.0",
-	packages=find_packages(exclude=['*test']),
-	install_requires=['matplotlib', 'networkx', 'numpy', 'pandas', 'oct2py', 'tqdm', 'scipy']
+    name="CodeEvolution",
+    version="1.0.0",
+    packages=find_packages(exclude=['*test']),
+    install_requires=['matplotlib', 'networkx',
+                      'numpy', 'pandas', 'tqdm', 'scipy']
 )
