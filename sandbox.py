bd7eba43bb04a0592dfd46bc037cb8faf1f9cdd1
ShirsenduP <s.podder@live.co.uk>
2020-04-06 22:59:46 +0100

    Had temporary script to generate data for LrGe vs updateProbability experiment

--- sandbox.py
+++ sandbox.py
@@ -11,37 +11,39 @@ pd.set_option('display.max_columns', 320)
 
 if __name__ == "__main__":
 
-    C = Config(
-        size=300,
-        initialState=State(0, 1, 8),
-        mutationProbability=0.1,
-        delta=1,
-        maxPeriods=2000)
+    for alpha in [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.99]:
+        C = Config(
+            size=300,
+            initialState=State(0, 1, 8),
+            mutationProbability=0.1,
+            delta=1,
+            sparseDensity=True,
+            updateProbability=alpha,
+            maxPeriods=2000)
 
-    variable = 'population'
-    strats = range(8)
-    values = Experiment.generateSinglePopulationProportionList(2, 8)
+        variable = 'population'
+        strats = range(8)
+        values = Experiment.generatePopulationList(range(8), 1, 8)
+        # values = Experiment.generateSinglePopulationProportionList(8, 8)
 
-    E = Experiment(
-        networkType=GrGeERNetwork,
-        description="Effect of size of population on the Leading 8 under Ohtsuki and Isawa",
-        variable=variable,
-        values=values,
-        defaultConfig=C,
-        repeats=3
-    )
+        E = Experiment(
+            networkType=LrGeERNetwork,
+            description="Effect of size of population on the Leading 8 under Ohtsuki and Isawa",
+            variable=variable,
+            values=values,
+            defaultConfig=C,
+            repeats=20
+        )
 
-    df = E.run(export=False, expName="s2_GrGe_Size")
+        E.showExperiments()
+        df = E.run(export=True, expName=f"LrGe_updateProb_{alpha}")
+        means = df.groupby('Main ID').mean()
+        sems = df.groupby('Main ID').sem()
 
-    # # df = pd.read_csv("s2_GrGe_Size.csv")
-    means = df.groupby('Mutant Initial Prop.').mean()
-    sems = df.groupby('Mutant Initial Prop.').sem()
-    plt.subplot()
-    plt.errorbar(x=np.arange(1, 0, -0.25), y=means['Prop. of Cooperators'], yerr=sems['Prop. of Cooperators'])
-    plt.ylabel("Proportion of Cooperation")
-    plt.xlabel("Size of Population")
+        plt.subplot()
+        plt.errorbar(x=strats, y=means['Prop. of Cooperators'], yerr=sems['Prop. of Cooperators'], label=f"{alpha}")
+        plt.ylabel("Proportion of Cooperation")
+        plt.xlabel("Strategy")
+
+    plt.legend()
     plt.show()
-    #
-    # # N = LrGeERNetwork(C)
-    # df = N.runSimulation()
-    # print(type(df))
