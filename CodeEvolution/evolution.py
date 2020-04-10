fdfd99df7e18cd823360a018dc001793edd3f9fb
ShirsenduP <s.podder@live.co.uk>
2020-04-10 17:36:43 +0100

    Reverted to PROPORTIONAL UTILITY Global Evolution and CONCURRENT Local Evolution

--- CodeEvolution/evolution.py
+++ CodeEvolution/evolution.py
@@ -19,32 +19,38 @@ class GlobalEvolution:
         bestStrategy = max(strategyUtils, key=lambda key: strategyUtils[key])
 
         # check for strategies with negative utility
-        # for strategy, utility in strategyUtils.items():
-        #     if utility < 0:
-        #         strategyUtils[strategy] = 0
+        for strategy, utility in strategyUtils.items():
+            if utility < 0:
+                strategyUtils[strategy] = 0
 
         # if total utility is zero, no evolutionary update
-        # totalUtil = sum(strategyUtils.values())
-        # if totalUtil == 0:
-        #     logging.debug(
-        #         f"\tupdate skipped because we have {strategyUtils.values()}")
-        #     return
+        totalUtil = sum(strategyUtils.values())
+        if totalUtil == 0:
+            logging.debug(
+                f"\tupdate skipped because we have {strategyUtils.values()}")
+            return
 
         # probability of switching to strategy i is (utility of strategy i)/(total utility of all non-negative
         # strategies)*(speed of evolution, larger the alpha, the slower the evolution)
-        # for strategy, _ in strategyUtils.items():
-        #     # strategyUtils[strategy] /= (totalUtil * alpha)
-        #     # strategyUtils[strategy] /= (totalUtil * 10)
-        #     strategyUtils[strategy] /= (totalUtil * self.config.size)
+        for strategy, _ in strategyUtils.items():
+            # strategyUtils[strategy] /= (totalUtil * alpha)
+            strategyUtils[strategy] /= (totalUtil * 10)
+            # strategyUtils[strategy] /= (totalUtil * self.config.size)
         # print(strategyUtils)
-        # logging.debug(
-        #     f"\tt = {self.currentPeriod}, probabilities are {strategyUtils}, best strategy is {bestStrategy}")
+        logging.debug(
+            f"\tt = {self.currentPeriod}, probabilities are {strategyUtils}, best strategy is {bestStrategy}")
 
         for agent in self.agentList:
-            if random.random() < self.config.alpha:
+            if random.random() < strategyUtils[bestStrategy]:
                 logging.debug(
                     f"Agent {agent.id} switched from {agent.Strategy.ID} to {bestStrategy}")
                 agent.Strategy.changeStrategy(bestStrategy)
+
+        # for agent in self.agentList:
+        #     if random.random() < self.config.alpha:
+        #         logging.debug(
+        #             f"Agent {agent.id} switched from {agent.Strategy.ID} to {bestStrategy}")
+        #         agent.Strategy.changeStrategy(bestStrategy)
 
 
 class LocalEvolution:
@@ -102,6 +108,4 @@ class LocalEvolution:
                 continue
             else:
                 agent.Strategy.changeStrategy(strategy_mapping[agent])
-
-
 
