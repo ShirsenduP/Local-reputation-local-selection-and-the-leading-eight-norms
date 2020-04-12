6e62a2441f3acf4d5f92391705e22751017edb4c
ShirsenduP <s.podder@live.co.uk>
2020-04-12 21:05:35 +0100

    Change to version 2020.04

--- sandbox.py
+++ sandbox.py
@@ -10,42 +10,29 @@ pd.set_option('display.width', 320)
 pd.set_option('display.max_columns', 320)
 
 if __name__ == "__main__":
-    for network in [LrLeERNetwork, GrLeERNetwork, GrGeERNetwork, LrGeERNetwork]:
-    # for network in [GrGeERNetwork, LrGeERNetwork]:
 
-        C = Config(
-            size=100,
-            initialState=State(0, 1, 8),
-            mutationProbability=0.1,
-            delta=1,
-            sparseDensity=True,
-            alpha=0.01,
-            maxPeriods=5000)
+    C = Config(
+        size=100,
+        initialState=State(0, 1, 8),
+        mutationProbability=0.1,
+        delta=1,
+        sparseDensity=True,
+        alpha=0.01,
+        maxPeriods=50)
 
-        variable = 'population'
-        strats = range(8)
-        values = Experiment.generatePopulationList(range(8), 1, 8)
-        # values = Experiment.generateSinglePopulationProportionList(8, 8)
+    variable = 'population'
+    strats = range(3)
+    values = Experiment.generatePopulationList(strats, 1, 8)
 
-        E = Experiment(
-            networkType=network,
-            description="Effect of size of population on the Leading 8 under Ohtsuki and Isawa",
-            variable=variable,
-            values=values,
-            defaultConfig=C,
-            repeats=20
-        )
+    E = Experiment(
+        networkType=GrGeERNetwork,
+        description="Effect of size of population on the Leading 8 under Ohtsuki and Isawa",
+        variable=variable,
+        values=values,
+        defaultConfig=C,
+        repeats=2
+    )
 
-        E.showExperiments()
-        df = E.run(export=False, expName=f"LrGe_updateProb_{0.1}")
-        means = df.groupby('Main ID').mean()
-        sems = df.groupby('Main ID').sem()
-
-        plt.subplot()
-        plt.errorbar(x=strats, y=means['Prop. of Cooperators'], yerr=sems['Prop. of Cooperators'], label=f"{0.1}")
-        plt.ylabel("Proportion of Cooperation")
-        plt.xlabel("Strategy")
-        plt.title(network.__name__)
-
-        plt.legend()
-        plt.show()
+    # E.showExperiments()
+    df = E.run(export=False, expName=f"LrGe_updateProb_{0.1}")
+    print(df)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CodeEvolution",
-    version="1.0.0",
+    version="2020.04",
     packages=find_packages(exclude=['*test']),
     install_requires=['matplotlib', 'networkx',
                       'numpy', 'pandas', 'tqdm', 'scipy']
