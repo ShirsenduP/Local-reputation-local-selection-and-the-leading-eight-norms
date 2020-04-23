26a63f66ac01e725c7f92eb427fb262a78e5f3d6
ShirsenduP <s.podder@live.co.uk>
2020-04-23 14:39:39 +0100

    updated micro version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CodeEvolution",
-    version="2020.04",
+    version="2020.04.01",
     packages=find_packages(exclude=['*test']),
     install_requires=['matplotlib', 'networkx',
                       'numpy', 'pandas', 'tqdm', 'scipy']
